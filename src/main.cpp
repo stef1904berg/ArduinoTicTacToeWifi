6f5414543b4321ee174a6d7fe0b808b2384bd967
stef1904berg <55401301+stef1904berg@users.noreply.github.com>
2023-11-15 23:17:44 +0100

    - handshake concept

--- src/main.cpp
+++ src/main.cpp
@@ -21,13 +21,20 @@ MqttClient mqtt(wifiClient);
 // 1: connecting to MQTT Server
 // 2: user host or join decision
 
-// 10: setup game hosting + listen for handshake request
-// 11: send handshake response
+// 10: broadcast open room + send handshake accept message
+// 11: finished handshakes
 
 // 20: listening for open rooms + send handshake request
-// 21: listen for handshake response
+// 21: finished handshakes
 int gameState = 0;
-int hostGame = 0;
+bool hostGame = false;
+
+// Game host variables
+bool handshakeSent = false;
+String clientId;
+
+// Client variables
+String hostId;
 
 // Button variables;
 int acceptButtonState;
@@ -37,13 +44,15 @@ int lastMoveButtonState = LOW;
 
 // Timers
 unsigned long previousMillis = 0;
-const long broadcastHostInterval = 1000;
+const long broadcastHostInterval = 10000;
 
-String clientIdentifier;
+String localId;
 
 void updateButtonStates();
 void publishMessage(const String& message, const String& topic);
 String randomString(int length);
+void onMqttMessage(int messageSize);
+String getValue(String data, char separator, int index);
 
 void setup() {
     Serial.begin(115200);
@@ -52,9 +61,9 @@ void setup() {
     int rSeed = analogRead(0) + (analogRead(0) * 1000) + (analogRead(0) * 1000000);
     randomSeed(rSeed);
 
-    clientIdentifier = randomString(10);
+    localId = randomString(10);
     Serial.print("Client identifier: ");
-    Serial.println(clientIdentifier);
+    Serial.println(localId);
 
     // Setup input buttons
     pinMode(ACCEPT_BUTTON_PIN, INPUT_PULLUP); // Accept button
@@ -63,6 +72,7 @@ void setup() {
     matrix.begin();
 
     mqtt.setUsernamePassword(MQTT_USERNAME, MQTT_PASSWORD);
+    mqtt.onMessage(onMqttMessage);
 
     if (WiFi.status() == WL_NO_MODULE) {
         Serial.println("Communication with WiFi module failed!");
@@ -75,9 +85,7 @@ void setup() {
 void loop() {
     updateButtonStates();
 
-    if (mqtt.connected()) {
-        mqtt.poll();
-    }
+    mqtt.poll();
 
     if (gameState == 0) {
         int status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -108,7 +116,8 @@ void loop() {
             while (true);
         }
 
-        publishMessage("connected", "device/" + WiFi.localIP().toString() + "/status");
+        publishMessage("connected", "device/" + localId + "/status");
+
         gameState = 2;
     }
 
@@ -126,13 +135,14 @@ void loop() {
 
         if (acceptButtonState == LOW && lastAcceptButtonState == HIGH) {
             if (hostGame) {
-                publishMessage("hosting", "device/" + WiFi.localIP().toString() + "/status");
+                publishMessage("hosting", "device/" + localId + "/status");
+                mqtt.subscribe(topicPrefix + "/rooms/" + localId + "/handshake");
                 gameState = 10;
             } else {
-                publishMessage("joining", "device/" + WiFi.localIP().toString() + "/status");
+                publishMessage("joining", "device/" + localId + "/status");
+                mqtt.subscribe(topicPrefix + "/rooms/+/status");
                 gameState = 20;
             }
-            mqtt.endMessage();
             board.clearFrame();
         }
     }
@@ -143,12 +153,25 @@ void loop() {
         if (currentMillis - previousMillis >= broadcastHostInterval) {
             previousMillis = currentMillis;
 
-            publishMessage("open", "rooms/" + clientIdentifier + "/status");
+            publishMessage("open", "rooms/" + localId + "/status");
 
             board.loadFrame(WIFI_CONNECTED_SCREEN);
         } else {
             board.clearFrame();
         }
+    }
+
+    if (gameState == 11) {
+        Serial.println("Host handshake complete");
+        publishMessage("connected", "/game/" + localId + clientId + "/client");
+        while (true);
+    }
+
+    if (gameState == 21) {
+        Serial.println("Client handshake complete");
+        publishMessage("connected", "/game/" + hostId + localId + "/client");
+        while (true);
+
     }
 
     lastAcceptButtonState = acceptButtonState;
@@ -169,10 +192,60 @@ void publishMessage(const String& message, const String& topic) {
 }
 
 String randomString(int length) {
-    const char possible[] = "abcdefghijklmnopqrstuvwxyz";
+    const char possible[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     char rString[length];
     for (int i = 0; i < length; ++i) {
-        rString[i] = possible[random(0, 26)];
+        rString[i] = possible[random(0, 52)];
     }
     return rString;
+}
+
+void onMqttMessage(int messageSize) {
+    String message;
+    String messageTopic = mqtt.messageTopic();
+
+    while (mqtt.available()) {
+        message += (char) mqtt.read();
+    }
+
+    Serial.println(messageTopic);
+    String firstPart = getValue(messageTopic, '/', 1);
+    if (firstPart == "rooms") {
+        String roomId = getValue(messageTopic, '/', 2);
+
+        String secondPart = getValue(messageTopic, '/', 3);
+        if (secondPart == "status" && message == "open") {
+            // Respond to open room message
+            // rooms/<room>/handshake = client id
+            publishMessage(localId, "rooms/" + roomId + "/handshake");
+            mqtt.subscribe(topicPrefix + "/" + roomId + "/" + localId);
+
+        } else if (secondPart == "handshake" && roomId == localId && hostGame && !handshakeSent) {
+            // Response to client room handshake request
+            // rooms/<room>/<client id> = accept
+            publishMessage("accept", "rooms/" + roomId + "/" + message);
+            handshakeSent = true;
+            gameState = 11;
+        } else if (secondPart == localId && message == "accept") {
+            hostId = roomId;
+            gameState == 21;
+        }
+    }
+}
+
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = {0, -1};
+    int maxIndex = data.length()-1;
+
+    for(int i=0; i<=maxIndex && found<=index; i++){
+        if(data.charAt(i)==separator || i==maxIndex){
+            found++;
+            strIndex[0] = strIndex[1]+1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+
+    return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
